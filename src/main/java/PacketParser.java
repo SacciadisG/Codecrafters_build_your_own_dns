1bf9a22d0a8700af6ac4b2280dbd00f2db851f24
Greg Sacciadis <greg.sacciadis@gmail.com>
2026-01-27 23:14:55 -0500

    CLI tests (cli-test-1769573695179)

--- src/main/java/PacketParser.java
+++ src/main/java/PacketParser.java
@@ -1,3 +1,4 @@
+import java.io.ByteArrayOutputStream;
 import java.nio.ByteBuffer;
 
 public class PacketParser {
@@ -12,4 +13,45 @@ public class PacketParser {
         short arCount = buffer.getShort();
         return new PacketHeader(packetID, flags, qdCount, anCount, nsCount, arCount);
     }
+
+    public static PacketQuestion readPacketQuestionSection(ByteBuffer buffer) {
+        // Reading the domain name
+        ByteArrayOutputStream domainOut = new ByteArrayOutputStream();
+        while (true) {
+            byte len = buffer.get();
+            if (len == 0) break; // End of domain name
+            byte[] labelBytes = new byte[len];
+            buffer.get(labelBytes);
+            domainOut.write(len);
+            domainOut.writeBytes(labelBytes);
+        }
+        byte[] domainName = domainOut.toByteArray();
+
+        short field_type = buffer.getShort();
+        short field_class = buffer.getShort();
+        return new PacketQuestion(domainName, field_type, field_class);
+    }
+
+    public static PacketAnswer readPacketAnswerSection(ByteBuffer buffer) {
+        // Reading the domain name
+        ByteArrayOutputStream domainOut = new ByteArrayOutputStream();
+        while (true) {
+            byte len = buffer.get();
+            if (len == 0) break; // End of domain name
+            byte[] labelBytes = new byte[len];
+            buffer.get(labelBytes);
+            domainOut.write(len);
+            domainOut.writeBytes(labelBytes);
+        }
+        byte[] domainName = domainOut.toByteArray();
+
+        short field_type = buffer.getShort();
+        short field_class = buffer.getShort();
+        int field_ttl = buffer.getInt();
+        short field_rdlength = buffer.getShort();
+        int field_rdata = buffer.getInt();
+        return new PacketAnswer(domainName, field_type, field_class, field_ttl, field_rdlength, field_rdata);
+    }
+
+    
 }
